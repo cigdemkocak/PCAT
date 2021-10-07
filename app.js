dddf3e4f8d16929f969a310898b34c638311e71f
Çiğdem Koçak <kocakcigdem9@gmail.com>
2021-10-07 16:41:33 +0300

    Mongoose denendi

--- app.js
+++ app.js
@@ -9,19 +9,23 @@ app.set('view engine', 'ejs');
 
 //MIDDLEWARE
 app.use(express.static('public'));
+app.use(express.urlencoded({ encoded: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  
   res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  
   res.render('about');
 });
 app.get('/add', (req, res) => {
-  
   res.render('add');
+});
+
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
