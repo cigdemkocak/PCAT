fc30bb04f999839d76219c165394a36fbddb2e8c
Çiğdem Koçak <kocakcigdem9@gmail.com>
2021-10-09 16:30:04 +0300

    Edit işlemleri yapıldı

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const path = require('path');
 const ejs = require('ejs');
-const Photo = require('./models/Photo')
+const fs = require('fs');
+const Photo = require('./models/Photo');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/pcat-tes-db' , {
-  useNewUrlParser : true,
-  
+mongoose.connect('mongodb://localhost/pcat-tes-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 //TEMPLATE ENGINE
@@ -18,21 +21,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
-  res.render('index' , {
-    photos
+  const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
   });
 });
 app.get('/photos/:id', async (req, res) => {
   //console.log(req.params.id)
   //res.render('about');
-  const photo = await Photo.findById(req.params.id)
+  const photo = await Photo.findById(req.params.id);
   res.render('photo', {
-    photo
-  })
+    photo,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -43,10 +47,42 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  //await Photo.create(req.body)
+  //res.redirect('/')
+
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title
+  photo.description = req.body.description
+  photo.save()
+
+  res.redirect(`/photos/${req.params.id}`)
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
